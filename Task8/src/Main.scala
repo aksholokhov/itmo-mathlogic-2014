d3b8207ffbdfb209886ac710634fe1477faea6e8
Alexey Sholokhov <ak.sholokhov@gmail.com>
2015-05-06 01:16:41 +0300

    Task8 fixed (architecture and minpor typos)

--- Task8/src/Main.scala
+++ Task8/src/Main.scala
@@ -2,13 +2,13 @@ import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-    //tester("tests.txt")
+   // tester("tests.txt")
     val str = Source.fromFile("test2").getLines().toArray
     val parser = new OrdinalParser
     val a = parser.parseAll(parser.expr, str(0).split("\\=")(0)).get
     val b = parser.parseAll(parser.expr, str(0).split("\\=")(1)).get
     val z = Atom(0)
-    z.ordToCantor(a).compare(z.ordToCantor(b)) match {
+    oToC(a).compare(oToC(b)) match {
       case 0 => println("Равны")
       case _ => println("Не равны")
     }
@@ -22,12 +22,20 @@ object Main {
       .map(s => test(s._1, s._2, s._3)).foreach(println(_))
   }
 
+  def oToC(o: Ordinal): CNF = o match {
+      case W() => CList(List((Atom(1), 1)), Atom(0))
+      case Num(a) => Atom(a)
+      case add(a, b) => oToC(a) + oToC(b)
+      case mul(a, b) => oToC(a) * oToC(b)
+      case pow(a, b) => oToC(a) ^ oToC(b)
+    }
+
   def test(a: Ordinal, true_ans: String, b: Ordinal): String = {
     val ords: String = a + " " + true_ans + " " + b
     val z = Atom(0)
     try {
-      val cantorA = z.ordToCantor(a)
-      val cantorB = z.ordToCantor(b)
+      val cantorA = oToC(a)
+      val cantorB = oToC(b)
       var myAns: String = ""
       cantorA.compare(cantorB) match {
         case n if n > 0 => myAns = ">"
@@ -35,14 +43,13 @@ object Main {
         case 0 => myAns = "="
       }
       if (myAns.equals(true_ans)) {
-        "YES: " + ords
+        "YES: " + cantorA + " " + true_ans + " " + cantorB
       }
       else {
-        "NO:  " + ords + ", found: " + myAns + ";"
+        "NO:  " + cantorA + " " + true_ans + " " + cantorB + ", found: " + myAns + ";"
       }
     } catch {
       case n: Exception => "ERR: " + ords + ", found: "  + n
-     // case n: Error => "ERR: " + ords + ", found: "  + n
     }
   }
 
--- Task8/src/Ordinal.scala
+++ Task8/src/Ordinal.scala
@@ -21,208 +21,177 @@
     override def toString: String = "w"
   }
 
-  case class Atom(n: Int) extends CNF {
-    override def toString: String = "(" + n.toString + ")"
-  }
-
-
   case class CList(list: List[(CNF, Int)], atom: Atom) extends CNF {
-    if (list.size == 0) throw new Exception("nil in CList")
-  /*  override def toString: String =
-      list.map(p => "(w " + (if (p._1 == Atom(1)) ""
-      else "^(" + p._1.toString + ")"
-        + (if (p._2 == 1) "" else "*" + p._2.toString)
-        + ")"))
-        .mkString("+") + (if (atom == Atom(0)) "" else "+" + atom.toString) */
-
+    override def toString = list.map(a => "(w" +
+        (if (a._1 != Atom(1)) "^(" + a._1.toString + ")" else "")
+        + ")"
+        + (if (a._2 != 1) "*" + a._2.toString else ""))
+        .mkString("+") + (if (atom != Atom(0)) "+" + atom.toString else "")
   }
 
+  case class Atom(Int: Int) extends CNF {
+    override def toString = Int.toString
+  }
 
-
-
-  sealed trait CNF extends Ordered[CNF] {
-
-    def ordToCantor(o: Ordinal): CNF = o match {
-      case W() => CList(List((Atom(1), 1)), Atom(0))
-      case Num(a) => Atom(a)
-      case add(a, b) => ordToCantor(a) + ordToCantor(b)
-      case mul(a, b) => ordToCantor(a) * ordToCantor(b)
-      case pow(a, b) => ordToCantor(a) ^ ordToCantor(b)
+  object CNF {
+    def addPair(pair: (CNF, Int), that: CNF) = that match {
+      case t@Atom(n) => CList(List((pair._1, pair._2)), t)
+      case CList(list, a) => CList((pair._1, pair._2) :: list, a)
     }
 
-    def compare(that: CNF): Int = this match {
+    def exp1(p: Atom, b: CNF): CNF = b match {
+      case a if b.fe == Atom(1) => CList(List((Atom(b.fc), p.Int ^ b.rest.asInstanceOf[Atom].Int)), Atom(0))
+      case a if b.rest.isInstanceOf[Atom] => CList(List((CList(List((b.fe - Atom(1), b.fc)), Atom(0)), p.Int ^ b.rest.asInstanceOf[Atom].Int)), Atom(0))
+      case _ =>
+        val c = exp1(p, b.rest)
+        CList(List((addPair((b.fe - Atom(1), b.fc), c.fe),c.fc)), Atom(0))
+    }
+
+    def exp2(a: CNF, q: Atom): CNF = q match {
+      case Atom(1) => a
+      case Atom(n) => CList(List((a.fe * Atom(n - 1), 1)), Atom(0)) * a
+    }
+
+    def exp3(a: CNF, q: Atom): CNF = q match {
+      case Atom(0) => Atom(1)
+      case Atom(1) => a
+      case x if a.limitp => exp2(a, q)
+      case Atom(n) => exp3(a, Atom(n - 1)) * a
+    }
+
+    def exp4(a: CNF, b: CNF): CNF = CList(List((a.fe * b.limitpart, 1)), Atom(0)) * exp3(a, b.Intpart)
+  }
+
+  sealed trait CNF extends Ordered[CNF]{
+    import CNF._
+    override def compare(that: CNF): Int = this match
+    {
       case Atom(n) => that match {
-        case Atom(m) => n.compareTo(m)
+        case Atom(m) => n compare m
         case _ => -1
       }
       case CList(_, _) if (that match {
         case Atom(_) => true
         case _ => false
       }) => 1
-      case CList(_, _) if fe(this) != fe(that) => fe(this).compare(fe(that))
-      case CList(_, _) if fc(this) != fc(that) => fc(this).compare(fc(that))
-      case _ => rest(this).compare(rest(that))
+      case CList(_, _) if this.fe != that.fe => this.fe compare that.fe
+      case CList(_, _) if this.fc != that.fc => this.fc compare that.fc
+      case _ => this.rest compare that.rest
     }
 
-
-    def +(b: CNF): CNF = this match {
-      case Atom(n) if (b match {
-        case Atom(m) => true
-        case _ => false
-      }) => Atom(n + b.asInstanceOf[Atom].n)
-      case _ => if (fe(this) < fe(b)) b
-      else if (fe(this) == fe(b)) addPair((fe(this), fc(this) + fc(b)), rest(b))
-      else addPair((fe(this), fc(b)), rest(this) + b)
-
+    def first: CNF = this match {
+      case a: Atom => this
+      case CList(list, a) => CList(list.head :: Nil, Atom(0))
     }
 
-    def -(b: CNF): CNF = this match {
-      case Atom(n) => b match {
-        case Atom(m) => if (n <= m) Atom(0) else Atom(n - m)
-        case _ => throw new Exception("Atom - NonAtom: " + this + "-" + b)
-      }
-      case a if fe(a) < fe(b) => Atom(0)
-      case a if fe(a) > fe(b) => a
-      case a if fc(a) < fc(b) => Atom(0)
-      case a if fc(a) > fc(b) => addPair((fe(a), fc(a) - fc(b)), rest(a))
-      case _ => rest(this) - rest(b)
+    def rest: CNF = this match {
+      case CList(list, a) if list.length == 1 => a
+      case CList(list, a) => CList(list.tail, a)
     }
 
-    def *(b: CNF): CNF = b match {
-      case Atom(0) => Atom(0)
-      case _ if this == Atom(0) => Atom(0)
-      case Atom(m) => this match {
-        case Atom(n) => Atom(n * m)
-        case a => addPair((fe(a), fc(a) * m), rest(a))
-      }
-      case a => addPair((fe(a) + fe(b), fc(b)), a * rest(b))
-    }
-
-    def ^(b: CNF) = this match {
-      case Atom(1) => Atom(1)
-      case _ if b == Atom(0) => Atom(1)
-      case t@Atom(n) => b match {
-        case Atom(m) => Atom(n ^ m)
-        case _ => exp1(t, b)
-      }
-      case _ if b.isInstanceOf[Atom] => exp3(this, b.asInstanceOf[Atom])
-      case _ => exp4(this, b)
-    }
-
-
-    def first(a: CNF): CNF = a match {
-      case e: Atom => e
-      case CList(list, p) => CList(list.head :: Nil, Atom(0))
-    }
-
-    def rest(a: CNF): CNF = a match {
-      case e: Atom => throw new Exception("atom in rest")
-      case CList(list, p) if list.length == 1 => p //WHY?
-      case CList(list, p) => CList(list.tail, p)
-    }
-
-    def firstn(a: CNF, n: Int): CNF = a match {
-      case t: Atom => t
-      case CList(list, _) => n match {
-        case _ if n > 1 => rest(firstn(a, n - 1)) match {
-          case t: Atom => CList(list, t)
-          case CList(list2, t) => CList(list ::: list2, t)
+    def firstn(n: Int): CNF = this.first match {
+      case t : Atom => t
+      case CList(l, _) => if (n > 1)
+        this.rest.firstn(n - 1) match {
+          case t : Atom => CList(l, t)
+          case CList(l2, a) => CList(l ::: l2, a)
         }
-        case 1 => rest(a) match {
-          case t: Atom => CList(list, t)
-          case CList(list2, _) => CList(list ::: list2, Atom(0))
-        }
-        case _ => throw new Exception("n = 0 in firstn")
-      }
+      else
+      if (n == 1) this.rest match {
+        case t@Atom(_) => CList(l, t)
+        case CList(l2, a) => CList(l ::: l2, Atom(0))
+      } else throw new Exception("firstn")
     }
 
-    def restn(a: CNF, n: Int): CNF = n match {
-      case 0 => a
-      case _ => rest(restn(a, n - 1))
+    def restn(n: Int): CNF = n match {
+      case 0 => this
+      case i => this.rest.restn(i - 1)
     }
 
-    def fe(a: CNF): CNF = a match {
+    def fe: CNF = this match {
       case Atom(_) => Atom(0)
       case CList(list, _) => list.head._1
     }
 
-    def fc(a: CNF): Int = a match {
+    def fc: Int = this match {
       case Atom(n) => n
       case CList(list, _) => list.head._2
     }
 
-    def length(a: CNF): Int = a match {
+    def length: Int = this match {
       case Atom(_) => 0
-      case CList(_, _) => length(rest(a)) + 1
+      case t@CList(_, _) => 1 + t.rest.length
     }
 
-    def size(a: CNF): Int = a match {
+    def size: Int = this match {
       case Atom(_) => 1
-      case p@CList(_, _) => size(fe(p)) + size(rest(p))
+      case t@CList(_, _) => t.fe.size + t.rest.size
     }
 
-    def limitp(a: CNF): Boolean = a match {
+    def limitp: Boolean = this match {
       case Atom(n) => n == 0
-      case t: CList => limitp(rest(t))
+      case t => t.rest.limitp
     }
 
-    def limitpart(a: CNF): CNF = a match {
+    def limitpart: CNF = this match {
       case Atom(n) => Atom(0)
-      case CList(_, _) => first(this) match {
-        case CList(list, _) => limitpart(rest(this)) match {
-          case n: Atom => CList(list, n)
-          case CList(list2, n) => CList(list ::: list2, n)
+      case CList(_, _) => this.first match {
+        case CList(l, _) => this.rest.limitpart match {
+          case t@Atom(n) => CList(l, t)
+          case CList(l2, a) => CList(l ::: l2, a)
         }
       }
-      case _ => throw new Exception("Pattern matching error in limitpart")
     }
 
-    def natpart(a: CNF): Atom = a match {
+    def Intpart: Atom = this match {
       case t: Atom => t
-      case _ => natpart(rest(a))
+      case _ => this.rest.Intpart
     }
 
-    def addPair(pair: (CNF, Int), b: CNF): CNF = b match {
-      case t: Atom => CList(List(pair), t)
-      case CList(list, t) => CList(pair :: list, t)
-    }
-
-    def exp1(t: Atom, b: CNF): CNF = b match {
-      case a if fe(b) == Atom(1) => CList(
-        List((Atom(fc(b)), t.n ^ rest(b).asInstanceOf[Atom].n)),
-        Atom(0)
-      )
-      case a if rest(b).isInstanceOf[Atom] => CList(
-        List((
-          CList(
-            List((fe(b) - Atom(1), fc(b))),
-            Atom(0)),
-          t.n ^ rest(b).asInstanceOf[Atom].n)),
-        Atom(0))
+    def +(that: CNF): CNF = this match {
+      case Atom(n) if (that match {
+        case Atom(m) => true
+        case _ => false
+      }) => Atom(n + that.asInstanceOf[Atom].Int)
       case _ =>
-        val p: CNF = exp1(t, rest(b))
-        CList(
-          List((addPair((fe(b) - Atom(1), fc(b)), fe(p)), fc(p))),
-          Atom(0)
-        )
+        if (this.fe < that.fe) that
+        else if (this.fe == that.fe) addPair((this.fe, this.fc + that.fc), that.rest)
+        else addPair((this.fe, this.fc), this.rest + that)
     }
 
-    def exp2(a: CNF, q: Atom): CNF = q match {
-      case Atom(1) => a
-      case Atom(n) => CList(List((fe(a) * Atom(n - 1), 1)), Atom(0)) * a
+    def -(that: CNF): CNF = this match {
+      case Atom(n) if that.isInstanceOf[Atom] => that match {
+        case Atom(m) => Atom(if (n <= m) 0 else n - m)
+      }
+      case a if this.fe < that.fe => Atom(0)
+      case a if this.fe > that.fe => this
+      case a if this.fc < that.fc => Atom(0)
+      case a if this.fc > that.fc => addPair((this.fe, this.fc - that.fc), a.rest)
+      case _ => this.rest - that.rest
     }
 
-    def exp3(a: CNF, q: Atom): CNF = q match {
-      case Atom(0) => Atom(1)
-      case Atom(1) => a
-      case x if limitp(a) => exp2(a, q)
-      case Atom(n) => exp3(a, Atom(n - 1)) * a
+    def *(that: CNF): CNF = that match {
+      case Atom(0) => Atom(0)
+      case a if this == Atom(0) => Atom(0)
+      case Atom(m) => this match {
+        case Atom(n) => Atom(n * m)
+        case _ => addPair((this.fe, this.fc * m), this.rest)
+      }
+      case _ => addPair((this.fe + that.fe, that.fc), this * that.rest)
     }
 
-    def exp4(a: CNF, b: CNF): CNF = CList(List((fe(a) * limitpart(b), 1)), Atom(0)) * exp3(a, natpart(b))
+
+    def ^(that: CNF) = this match {
+      case Atom(1) => Atom(1)
+      case a if that == Atom(0) => Atom(1)
+      case Atom(0) => Atom(0)
+      case t@Atom(n) => that match {
+        case Atom(m) => Atom(n ^ m)
+        case _ => exp1(t, that)
+      }
+      case a if that.isInstanceOf[Atom] => exp3(this, that.asInstanceOf[Atom])
+      case _ => exp4(this, that)
+    }
   }
-
-
-
 
 
