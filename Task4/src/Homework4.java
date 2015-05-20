a8d9b8714fde5bf5181c5fc0f39b5a7481b5af2c
Alexey Sholokhov <ak.sholokhov@gmail.com>
2015-05-20 11:53:51 +0300

    file input fixed

--- Task4/src/Homework4.java
+++ Task4/src/Homework4.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Homework4 {
     public static void main(String[] args) {
-        String testFilename = "test6.out";
+        String testFilename = args[0];
 
         PredicateParser parser = new PredicateParser();
 
